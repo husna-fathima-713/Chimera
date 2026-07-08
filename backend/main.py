8501b17be891b6c9da5b17c3b9daa2dc297a691c
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-07-09 00:54:47 +0530

    Health Router

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
+from backend.api.routes.health import router as health_router
 from backend.config import APP_NAME, VERSION
-
 
 app = FastAPI(
     title=APP_NAME,
     version=VERSION,
 )
+
+app.include_router(health_router)
 
 
 @app.get("/")
@@ -15,10 +17,4 @@ def root():
         "name": APP_NAME,
         "status": "online",
         "version": VERSION,
-    }
-
-@app.get("/health")
-def health():
-    return {
-        "status": "healthy"
     }
