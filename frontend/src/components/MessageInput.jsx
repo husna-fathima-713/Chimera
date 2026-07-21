8dad6e8ffc92f95bdb651bf35010014d1d2c8b40
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-07-21 15:07:55 +0530

    feat: message input

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,5 +1,25 @@
 function MessageInput() {
-    return <div>Message Input</div>;
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Type a message..."
+                style={{
+                    width: "85%",
+                    padding: "10px"
+                }}
+            />
+
+            <button
+                style={{
+                    marginLeft: "10px",
+                    padding: "10px 20px"
+                }}
+            >
+                Send
+            </button>
+        </div>
+    );
 }
 
 export default MessageInput;
