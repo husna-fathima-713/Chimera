a262148d806fd49cf1bd1e26b7dc71b71924d070
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-07-22 19:13:59 +0530

    feat: update chat service

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -2,10 +2,18 @@ import api from "../api/api";
 
 export async function sendMessage(chatId, prompt) {
 
+    console.log("Calling backend...");
+    console.log({
+        chat_id: chatId,
+        prompt: prompt,
+    });
+
     const response = await api.post("/chat", {
         chat_id: chatId,
         prompt: prompt,
     });
+
+    console.log("Backend Response:", response);
 
     return response.data.response;
 }
