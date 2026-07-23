de65b53988690eb03c7bad39cb421740f2146c85
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-07-23 15:14:02 +0530

    feat: update document service

---
+++ frontend/src/services/documentService.js
@@ -0,0 +1,20 @@
+import api from "../api/api";
+
+export async function uploadDocument(file) {
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    const response = await api.post(
+        "/documents",
+        formData,
+        {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        }
+    );
+
+    return response.data;
+}
