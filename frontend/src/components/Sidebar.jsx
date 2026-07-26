3533417791421c1981568821ed60c1496ce038b2
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-07-26 20:33:39 +0530

    Improve frontend layout structure with chatlist sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,37 @@
+import ChatList from "./ChatList";
+
 function Sidebar() {
+
     return (
+
         <>
-            <h2>Chimera</h2>
 
-            <br />
+            <h2
+                style={{
+                    marginBottom: "20px"
+                }}
+            >
+                Chimera
+            </h2>
 
-            <button>+ New Chat</button>
+            <button
+                style={{
+                    padding: "12px",
+                    borderRadius: "8px",
+                    background: "#343541",
+                    color: "white",
+                    marginBottom: "20px"
+                }}
+            >
+                + New Chat
+            </button>
+
+            <ChatList />
+
         </>
+
     );
+
 }
 
 export default Sidebar;
