fba2cc22f1683043f760de93103b6afd74f1b99a
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-07-28 20:18:09 +0530

    Update chatlist

--- frontend/src/services/chatListService.js
+++ frontend/src/services/chatListService.js
@@ -1,25 +1,99 @@
-import api from "../api/api";
+import { useEffect, useState } from "react";
 
-export async function getChats() {
+import ChatItem from "./ChatItem";
 
-    const response = await api.get("/chats");
+import {
+    getChats,
+    getChat,
+} from "../services/chatListService";
 
-    return response.data;
+function ChatList({
+
+    chatId,
+    setChatId,
+    setMessages,
+    refreshChats
+
+}) {
+
+    const [chats, setChats] = useState([]);
+
+    async function loadChats() {
+
+        try {
+
+            const data = await getChats();
+
+            setChats(data);
+
+        }
+
+        catch (error) {
+
+            console.error(error);
+
+        }
+
+    }
+
+    useEffect(() => {
+
+        loadChats();
+
+    }, [refreshChats]);
+
+    async function selectChat(chat) {
+
+        try {
+
+            const data = await getChat(chat.id);
+
+            setChatId(chat.id);
+
+            setMessages(data.messages);
+
+        }
+
+        catch (error) {
+
+            console.error(error);
+
+        }
+
+    }
+
+    return (
+
+        <div
+            style={{
+                marginTop: "20px"
+            }}
+        >
+
+            {
+
+                chats.map(chat => (
+
+                    <ChatItem
+
+                        key={chat.id}
+
+                        chat={chat}
+
+                        active={chat.id === chatId}
+
+                        onClick={() => selectChat(chat)}
+
+                    />
+
+                ))
+
+            }
+
+        </div>
+
+    );
 
 }
 
-export async function getChat(chatId) {
-
-    const response = await api.get(`/chats/${chatId}`);
-
-    return response.data;
-
-}
-
-export async function createChat() {
-
-    const response = await api.post("/chats");
-
-    return response.data;
-
-}
+export default ChatList;
