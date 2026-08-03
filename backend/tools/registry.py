e3c8e51e34a1f033a26f2e59ad49256c635833a9
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-08-03 12:14:38 +0530

    Update tools in backend for registry

---
+++ backend/tools/registry.py
@@ -0,0 +1,33 @@
+from backend.tools.calculator import CalculatorTool
+
+
+class ToolRegistry:
+
+    def __init__(self):
+
+        self.tools = {}
+
+        self.register(
+            CalculatorTool()
+        )
+
+    def register(self, tool):
+
+        self.tools[tool.name] = tool
+
+    def get(self, name):
+
+        return self.tools.get(name)
+
+    def list_tools(self):
+
+        return [
+
+            {
+                "name": tool.name,
+                "description": tool.description
+            }
+
+            for tool in self.tools.values()
+
+        ]
