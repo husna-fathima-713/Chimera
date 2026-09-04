2624a73587619e5ce8bcd3ee9dbca62fac983295
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-09-04 23:34:58 +0530

    Integrate Finance Controller frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./styles/global.css";
+import "./App.css";
 
 ReactDOM.createRoot(
     document.getElementById("root")
 ).render(
-
     <React.StrictMode>
         <App />
     </React.StrictMode>
-
 );
